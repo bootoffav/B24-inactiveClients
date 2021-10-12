18a52cab3a919d2b31cb65144d70d6126171e60c
Aleksei Butov <bootoffav@gmail.com>
2021-10-12 17:57:56 +0800

    Add netlify identity

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bulma";
+import netlifyIdentity from "netlify-identity-widget";
 import "./styles.scss";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+netlifyIdentity.init();
+
+netlifyIdentity.on("login", () => {
+  netlifyIdentity.close();
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
+
+netlifyIdentity.currentUser()
+  ? ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById("root")
+    )
+  : netlifyIdentity.open("login");
