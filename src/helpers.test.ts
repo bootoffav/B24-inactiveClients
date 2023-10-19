8517a1e50612a424405328f7e70f547106d5377c
Aleksei Butov <bootoffav@gmail.com>
2023-10-19 12:27:33 +0300

    Add Anzelika to inactive clients

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -1,16 +1,17 @@
 import { getLastDayOfActivePeriod, isManager } from "./helpers";
 
-it("get last day of active period", () => {
-  const lastDay = getLastDayOfActivePeriod("6", "months");
+// it("get last day of active period", () => {
+// const lastDay = getLastDayOfActivePeriod("6", "months");
 
-  //   console.log(lastDay);
-  //   expect(lastDay.format("YYYY-MM-DD")).toBe("2021-05-11");
-});
+//   console.log(lastDay);
+//   expect(lastDay.format("YYYY-MM-DD")).toBe("2021-05-11");
+// });
 
 it("checks if User is a Manager", () => {
   expect(isManager()).toBe(false);
   expect(isManager("maxim@xmtextiles.com")).toBe(true);
   expect(isManager("admin@xmtextiles.com")).toBe(true);
+  expect(isManager("anzelika@xmtextiles.eu")).toBe(true);
   expect(isManager("lilija@xmtextiles.eu")).toBe(false);
   expect(isManager("jurgita@xmtextiles.eu")).toBe(false);
 });
